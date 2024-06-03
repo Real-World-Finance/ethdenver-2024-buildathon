063c889ec26b4bdf7803ddb0c5eb6b6fdc3591be
Iván Baldo <ibaldo@netlabs.com.uy>
2024-06-03 16:50:50 -0300

    New NFT-PoI.sol contract from current OpenZeppelin without changes.
    Trying to modernize and have the minimal amount of changes needed for our purpose:
    * to see the NFTs on the wallet (Metamask mainly)
    * to see the NFTs on OpenSea

--- packages/hardhat/contracts/NFT-PoI.sol
+++ packages/hardhat/contracts/NFT-PoI.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTPoI is ERC721, ERC721Burnable, Ownable, IERC721Enumerable {
+/// @custom:security-contact contacto@netlabs.com.uy
+contract NFTPoI is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
-    mapping(address => uint256[]) private _ownedTokens;
 
     constructor(address initialOwner)
         ERC721("NFT-PoI", "POI")
@@ -17,30 +17,37 @@ contract NFTPoI is ERC721, ERC721Burnable, Ownable, IERC721Enumerable {
     {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.io/ipfs/QmPNxZ7nY1vAuqyGm639FExegAqKj4qLoihNwnQqZGpKoG/";
+        return "https://ipfs.io/ipfs/QmYBwZzYjCrjUFkEGiRMLRi3P4ztmMc9dp7XhmA1Zaie6j/?";
     }
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _ownedTokens[to].push(tokenId);
     }
 
-    function getNFTs(address owner) public view returns (uint256[] memory) {
-        return _ownedTokens[owner];
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
     }
 
-    function tokenOfOwnerByIndex(address owner, uint256 index) public view override returns (uint256) {
-        require(index < balanceOf(owner), "ERC721Enumerable: owner index out of bounds");
-        return _ownedTokens[owner][index];
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
     }
 
-    function totalSupply() public view override returns (uint256) {
-        return _nextTokenId;
-    }
-
-    function tokenByIndex(uint256 index) public view override returns (uint256) {
-        require(index < totalSupply(), "ERC721Enumerable: global index out of bounds");
-        return index;
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
